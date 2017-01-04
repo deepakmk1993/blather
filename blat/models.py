95e4c09d9d11277faff24235fd1bf8c9dc309864
Monkey D. Luffy <deepakgupta.ultimate@gmail.com>
2017-01-04 06:34:15 +0000

    Initialize repository

--- blat/models.py
+++ blat/models.py
@@ -3,3 +3,16 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Blat(models.Model):
+  text = models.TextField()
+  created_on = models.DateTimeField(auto_now_add=True)
+  via = models.URLField(blank=True)
+  
+  def total_likes(self):
+    return self.like_set.count()
+  
+  def __unicode__(self):
+    return self.text[:50]
+  
+class Like(models.Model):
+  blat = models.ForeignKey(Blat)
