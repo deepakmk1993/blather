3d01a51981fd49e600abc204b737e38cc2955174
Monkey D. Luffy <deepakgupta.ultimate@gmail.com>
2017-01-04 17:22:56 +0000

    Initialize repository

--- blat/models.py
+++ blat/models.py
@@ -18,3 +18,8 @@ class Blat(models.Model):
   
 class Like(models.Model):
   blat = models.ForeignKey(Blat)
+
+class Profile(models.Model):
+  user = models.OneToOneField(User)
+  bio = models.TextField(blank=True)
+  blog = models.URLField(blank=True)
